6c3a47612791687b8d22eb284123c00f16cf54e6
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-11-02 09:41:48 +0100

    Showing surface (with fake camera).

--- src/TerraGlide/Event.hs
+++ src/TerraGlide/Event.hs
@@ -1,13 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
 module TerraGlide.Event
     ( onEvent
     ) where
 
-import           Scene            (Event (..), Viewer, close)
-import           TerraGlide.State (State)
+import           Flow             ((<|))
+import           Graphics.GL      (GLfloat)
+import           Linear           (M44, V3 (..), (!*!))
+import           Scene
+import qualified Scene.Camera     as Camera
+import           Scene.Math       (Angle (..), mkPerspectiveMatrix,
+                                   mkViewMatrix)
+import           TerraGlide.State (State (..))
 
 onEvent :: Viewer -> Event -> Maybe State -> IO (Maybe State)
 
-onEvent _viewer (Frame _ _) (Just state) =
+onEvent viewer (Frame _ viewport) (Just state) = do
+    let perspMatrix = mkPerspectiveMatrix (Degrees 45) viewport 1 100
+        --viewMatrix = Camera.matrix <| mainCamera state
+        viewMatrix = mkViewMatrix (V3 0 3 10) (V3 0 0 0) (V3 0 1 0)
+        mvpMatrix = perspMatrix !*! viewMatrix :: M44 GLfloat
+
+    setScene viewer <|
+        Scene
+            { sceneSettings =
+                [ Clear [ColorBufferBit, DepthBufferBit]
+                ]
+            , sceneEntities =
+                [ Entity
+                    { entitySettings = []
+                    , entityProgram = dummyProgram state
+                    , entityMesh = dummyMesh state
+                    , entityUniforms =
+                        [ UniformValue "mvpMatrix" mvpMatrix
+                        ]
+                    , entityTextures = []
+                    }
+                ]
+            }
     return (Just state)
 
 onEvent viewer CloseRequest (Just state) = do
