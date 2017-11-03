b4e1efbee9c11ac7b7072921485705d54e5a6dd1
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-11-03 10:52:47 +0100

    Refactoring.

--- src/TerraGlide/Event.hs
+++ src/TerraGlide/Event.hs
@@ -10,9 +10,30 @@ import qualified Scene.Camera     as Camera
 import           Scene.Math       (Angle (..), mkPerspectiveMatrix)
 import           TerraGlide.State (State (..))
 
+-- | Dispatch 'Event'.
 onEvent :: Viewer -> Event -> Maybe State -> IO (Maybe State)
 
-onEvent viewer (Frame _ viewport) (Just state) = do
+-- Dispatch to local handler.
+onEvent viewer frame@(Frame _duration _viewport) (Just state) =
+    Just <$> onFrame viewer frame state
+
+-- Dispatch to local handler.
+onEvent viewer key@(KeyStroke _key _keyState _modifierKeys) (Just state) =
+    Just <$> onKeyStroke viewer key state
+
+-- Close.
+onEvent viewer CloseRequest (Just state) = do
+    close viewer
+    return (Just state)
+
+-- Init went wrong, close.
+onEvent viewer _ Nothing = do
+    close viewer
+    return Nothing
+
+-- | Handle the Frame event.
+onFrame :: Viewer -> Event -> State -> IO State
+onFrame viewer (Frame _ viewport) state = do
     let perspMatrix = mkPerspectiveMatrix (Degrees 45) viewport 1 100
         viewMatrix = Camera.matrix <| mainCamera state
         mvpMatrix = perspMatrix !*! viewMatrix
@@ -34,17 +55,12 @@ onEvent viewer (Frame _ viewport) (Just state) = do
                     }
                 ]
             }
-    return (Just state)
+    return state
+onFrame _ _ _ = error "Shall never happen"
 
-onEvent viewer (KeyStroke key keyState _) s@(Just _state) = do
+-- | Handle the KeyStroke event.
+onKeyStroke :: Viewer -> Event -> State -> IO State
+onKeyStroke viewer (KeyStroke key keyState _) state = do
     sceneLog viewer <| toLogStr ("KeyStroke: " ++ show key ++ ", " ++ show keyState)
-    return s
-
-onEvent viewer CloseRequest (Just state) = do
-    close viewer
-    return (Just state)
-
--- A previous handler, init or event, have returned Nothing due to problems.
-onEvent viewer _ Nothing = do
-    close viewer
-    return Nothing
+    return state
+onKeyStroke _ _ _ = error "Shall never happen"
