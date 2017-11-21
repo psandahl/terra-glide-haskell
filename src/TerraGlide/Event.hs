05e4fd3ac3f168a3f34e9d750a0ed989412bd003
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-11-21 13:38:23 +0100

    Adjusted near plane.

--- src/TerraGlide/Event.hs
+++ src/TerraGlide/Event.hs
@@ -52,7 +52,7 @@ onEvent viewer _ Nothing = do
 -- | Handle the Frame event.
 onFrame :: Viewer -> Event -> State -> IO State
 onFrame viewer (Frame duration viewport) state = do
-    let perspMatrix = mkPerspectiveMatrix (Degrees 45) viewport 1 100
+    let perspMatrix = mkPerspectiveMatrix (Degrees 45) viewport 0.1 100
         newCamera = CameraNavigation.animate (realToFrac duration)
                                              (state ^. mainCameraNavigation)
                                              (state ^. mainCamera)
