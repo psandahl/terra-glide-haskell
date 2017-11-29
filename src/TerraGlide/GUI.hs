bef41b018f2e01ab81dfd04ff292e77b3b0933d2
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-11-29 12:38:28 +0100

    Translating the rear mirror.

--- src/TerraGlide/GUI.hs
+++ src/TerraGlide/GUI.hs
@@ -7,7 +7,8 @@ module TerraGlide.GUI
 
 import           Data.Vector.Storable                (Vector, fromList)
 import           Flow                                ((<|))
-import           Linear                              (M44, V2 (..), V3 (..))
+import           Linear                              (M44, V2 (..), V3 (..),
+                                                      (!*!))
 import           Prelude                             hiding (init)
 import           Scene                               hiding (texture)
 import qualified Scene.GL.Attribute.VertexWithPosTex as WithPosTex
@@ -36,7 +37,9 @@ getRearMirrorEntity :: Viewport -> Viewport -> Texture -> GUI -> Entity
 getRearMirrorEntity viewport1 viewport2 texture gui =
     let reverseRatio = fromIntegral (height viewport1) / fromIntegral (width viewport1)
         ratio = fromIntegral (width viewport2) / fromIntegral (height viewport2)
-        modelMatrix = mkScalingMatrix <| V3 0.5 (0.5 * reverseRatio * ratio) 0
+        translation = mkTranslationMatrix <| V3 (-0.7) 0.7 0
+        scaling = mkScalingMatrix <| V3 0.4 (0.4 * reverseRatio * ratio) 0
+        modelMatrix = translation !*! scaling
     in Entity
            { entitySettings =
                [ SetDepthFunc Always
